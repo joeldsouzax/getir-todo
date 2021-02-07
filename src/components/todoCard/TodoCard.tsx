fd06911915202cbe3d781dbadd8be3cf338599f8
Joel DSouza <joel.dsouza@seamless.se>
2021-02-07 18:36:30 +0300

    added animation for the todo list

--- src/components/todoCard/TodoCard.tsx
+++ src/components/todoCard/TodoCard.tsx
@@ -1,10 +1,15 @@
 import * as React from "react";
 import { Card, Title } from "../design/DesignSystem";
 
-const TodoCard: React.FC = () => {
+interface TodoCardProps {
+  children: React.ReactNode;
+  style?: React.CSSProperties;
+}
+
+const TodoCard: React.FC<TodoCardProps> = ({ children, style }) => {
   return (
-    <Card width="40%">
-      <Title>Todo Title</Title>
+    <Card style={style} width="400px">
+      <Title>{children}</Title>
     </Card>
   );
 };
--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -5,19 +5,31 @@ import { useTrail, animated } from "react-spring";
 const Todo: React.FC = () => {
   const [todos, setTodos] = React.useState<Array<string>>([]);
 
+  const trail = useTrail(todos.length, {
+    config: { mass: 5, tension: 2000, friction: 200 },
+    opacity: 1,
+    x: 0,
+    height: 110,
+    from: { opacity: 0, x: 20, height: 0 },
+  });
+
   React.useEffect(() => {
     window.setTimeout(() => {
-      setTodos(["1", "2", "3", "4", ""]);
+      setTodos(["1", "2", "3", "4", "5", "6", "7"]);
     }, 2000);
   }, []);
 
   return (
     <React.Fragment>
-      <TodoCard />
-      <TodoCard />
-      <TodoCard />
-      <TodoCard />
-      <TodoCard />
+      {trail.map(({ x, height, ...rest }, index) => (
+        <animated.div
+          key={todos[index]}
+          className="todos-trail"
+          style={{ ...rest, transform: x.interpolate((x) => `translate3d(0,${x}px,0)`) }}
+        >
+          <TodoCard style={{ height: String(height) }}>{todos[index]}</TodoCard>
+        </animated.div>
+      ))}
     </React.Fragment>
   );
 };
